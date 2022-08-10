0e132aae95cbad0bd241f8e792563a71cc63400a
RestbaseDev <110992701+RestbaseDev@users.noreply.github.com>
2022-08-10 15:28:53 -0300

    Update index.php

--- main/index.php
+++ main/index.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    link rel="stylesheet" type="text/css" href="style.css">
+    <title>SalesSuit</title>
+  </head>
+  <body>
+  
+  </body>
+</html>
