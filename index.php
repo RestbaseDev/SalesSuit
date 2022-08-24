56222fd9e53d0ccde845383db353013daf9a48a9
RestbaseDev <110992701+RestbaseDev@users.noreply.github.com>
2022-08-24 12:33:29 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
+    <title>SalesSuit</title>
+  </head>
+  <body>
+    <main>
+      <section class="login">
+        <img src="img.jpg"" class="login_logo">
+        <h1 class="title_login">
+          Sales Sui
+        </h1>
+        
+        <label class="login_label">
+          <span>Usuario</span>
+          <input type="text" name="username" class="input">
+        </label>
+        
+        <label class="login_label">
+        </label>
+        
+        <div class="login_icons">
+
+        </div>
+        
+        <label class="login_label_checkbox">
+          <input type="checkbox" class="input_checkbox">
+          Manter login
+        </label>
+        
+        <button type="button" class="login_button" disabled>
+          
+        </button>
+      </section>
+       
+    </main>
+    
+  
+  </body>
+</html>
